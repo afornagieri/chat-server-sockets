46b6db060f1991ca57da36696a6cf32abaa14a08
afornagieri <fornagieri.artur@gmail.com>
2022-04-03 18:04:40 -0300

    Client

---
+++ Client/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.NumericUpDown();
+            this.txtNickname = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtChat = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.labelStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPort)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAddress.Location = new System.Drawing.Point(12, 12);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(392, 31);
+            this.txtAddress.TabIndex = 0;
+            this.txtAddress.Text = "127.0.0.1";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPort.Location = new System.Drawing.Point(419, 12);
+            this.txtPort.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.txtPort.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(175, 31);
+            this.txtPort.TabIndex = 1;
+            this.txtPort.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // txtNickname
+            // 
+            this.txtNickname.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNickname.Location = new System.Drawing.Point(12, 57);
+            this.txtNickname.Name = "txtNickname";
+            this.txtNickname.Size = new System.Drawing.Size(392, 31);
+            this.txtNickname.TabIndex = 2;
+            this.txtNickname.Text = "Visitor";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnect.Location = new System.Drawing.Point(419, 53);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(175, 39);
+            this.btnConnect.TabIndex = 3;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtChat
+            // 
+            this.txtChat.Enabled = false;
+            this.txtChat.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtChat.Location = new System.Drawing.Point(12, 398);
+            this.txtChat.Name = "txtChat";
+            this.txtChat.Size = new System.Drawing.Size(578, 31);
+            this.txtChat.TabIndex = 4;
+            this.txtChat.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtChat_KeyPress);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Enabled = false;
+            this.btnSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSend.Location = new System.Drawing.Point(596, 388);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(192, 50);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "SEND";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLog.Location = new System.Drawing.Point(12, 102);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(776, 280);
+            this.txtLog.TabIndex = 6;
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStatus.Location = new System.Drawing.Point(12, 456);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(776, 148);
+            this.labelStatus.TabIndex = 7;
+            this.labelStatus.Text = "Not connected";
+            this.labelStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 613);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtChat);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtNickname);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtAddress);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.txtPort)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.NumericUpDown txtPort;
+        private System.Windows.Forms.TextBox txtNickname;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtChat;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Label labelStatus;
+    }
+}
+
---
+++ Client/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        //Handle with user nickname
+        private string userName;
+        private StreamWriter swWriter;
+        private StreamReader srReader;
+        private TcpClient tcpClient;
+
+        //Needed to update log messages
+        private delegate void UpdateLogCallback(string message);
+
+        //Needed to define form state "disconnected" from another thread
+        private delegate void CloseConnectionCallback(string reason);
+        
+        private Thread messageThread;
+        private IPAddress iPAddress;
+        private int port;
+        private bool connected = false;
+
+        public Form1()
+        {
+            Application.ApplicationExit += new EventHandler(OnApplicationExit);
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (!connected)
+            {
+                initConnection();
+            }
+            else
+            {
+                CloseConnection("Desconnected by the user");
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            SendMessage();
+        }
+
+        private void txtChat_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                SendMessage();
+            }
+        }
+
+        private void initConnection()
+        {
+            try
+            {
+                iPAddress = IPAddress.Parse(txtAddress.Text);
+                port = (int)txtPort.Value;
+
+                tcpClient = new TcpClient();
+                tcpClient.Connect(iPAddress, port);
+
+                connected = true;
+
+                txtAddress.Enabled = false;
+                txtPort.Enabled = false;
+                txtNickname.Enabled = false;
+
+                txtChat.Enabled = true;
+                btnSend.Enabled = true;
+
+                btnConnect.ForeColor = Color.Red;
+                btnConnect.Text = "Disconnect";
+
+                swWriter = new StreamWriter(tcpClient.GetStream());
+                swWriter.WriteLine(txtNickname.Text);
+                swWriter.Flush();
+
+                messageThread = new Thread(new ThreadStart(receiveMessage));
+                messageThread.IsBackground = true;
+                messageThread.Start();
+
+                labelStatus.Invoke(new Action(() =>
+                {
+                    labelStatus.ForeColor = Color.Green;
+                    labelStatus.Text = $"Connected ! \n\n Host:{iPAddress} \n {port} \n";
+                }));
+            }
+            catch (Exception ex)
+            {
+                labelStatus.Invoke(new Action(() =>
+                {
+                    labelStatus.ForeColor = Color.Red;
+                    labelStatus.Text = "Error while trying to connect " + ex.Message;
+                }));
+            }
+        }
+
+        private void receiveMessage()
+        {
+            srReader = new StreamReader(tcpClient.GetStream());
+            string conResponse = srReader.ReadLine();
+
+            //0 == disconnected
+            if (conResponse[0] == '1')
+            {
+                this.Invoke(new UpdateLogCallback(this.UpdateLog), new object[] { "Connected with success !" });
+            }
+            else
+            {
+                string reason = "Not connected: ";
+                reason += conResponse.Substring(2, conResponse.Length - 2);
+                this.Invoke(new CloseConnectionCallback(this.UpdateLog), new object[] { reason });
+                return;
+            }
+
+            while (connected)
+            {
+                this.Invoke(new UpdateLogCallback(this.UpdateLog), new object[] { srReader.ReadLine() });
+            }
+        }
+
+        private void UpdateLog(string message)
+        {
+            txtLog.AppendText(message + "\r\n");
+        }
+
+        private void SendMessage()
+        {
+            if (txtChat.Lines.Length >= 1)
+            {
+                swWriter.WriteLine(txtChat.Text);
+                swWriter.Flush();
+
+                txtChat.Lines = null;
+            }
+
+            txtChat.Text = "";
+        }
+
+        private void CloseConnection(string reason)
+        {
+            txtLog.AppendText(reason + "\r\n");
+
+            txtAddress.Enabled = true;
+            txtPort.Enabled = true;
+            txtChat.Enabled = false;
+            btnSend.Enabled = false;
+
+            btnConnect.ForeColor = Color.Black;
+            btnConnect.Text = "Connect";
+
+            connected = false;
+
+            swWriter.Close();
+            srReader.Close();
+            tcpClient.Close();
+
+            labelStatus.Invoke(new Action(() =>
+            {
+                labelStatus.ForeColor = Color.Green;
+                labelStatus.Text = $"Deconnecting from... \n\n Server: {iPAddress} \n Port: {port} \n";
+            }));
+        }
+
+        public void OnApplicationExit(object sender, EventArgs e)
+        {
+            if (connected)
+            {
+                connected = false;
+
+                swWriter.Close();
+                srReader.Close();
+                tcpClient.Close();
+
+                labelStatus.Invoke(new Action(() =>
+                {
+                    labelStatus.ForeColor = Color.Green;
+                    labelStatus.Text = $"Deconnected from... \n\n Server: {iPAddress} \n Port: {port} \n";
+                }));
+            }
+        }
+    }
+}
